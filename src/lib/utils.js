8901bc887a0b097dc0371101e9d26236bdcc90d5
Nestor Santiago Fuhr <nestor.fuhr@gmail.com>
2022-04-04 10:07:58 -0300

    Create a pipe function.... pero yo no la voy a usar

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,3 +1,7 @@
 // junto los argumentos en un Rest Operator (comma separated list of args, budled in an []) 
 // y luego expando esos args con Spread Operator como args de fn
 export const partial = (fn, ...args) => fn.bind(null, ...args)
+
+
+// pipe function. NO LA VOY A USAR, HACE COMPLEJO EL CODIGO
+export const pipe = (f, g) => (...args) => g(f(...args))
--- src/lib/utils.test.js
+++ src/lib/utils.test.js
@@ -1,7 +1,11 @@
-import { partial } from './utils'
+import { partial, pipe } from './utils'
 
 const add = (a, b) => a + b
 const addThree = (a, b, c) => a + b + c;
+
+const inc = (num) => num + 1
+const dbl = (num) => num * 2
+
 
 test('partial applies the first argument ahead of time', () => {
   const inc = partial(add, 1)
@@ -13,4 +17,19 @@ test("partial applies the multiple arguments ahead of time", () => {
   const inc = partial(addThree, 1, 3);
   const result = inc(2); //expect 6
   expect(result).toBe(6);
+});
+
+
+// tests para la pipe utility. NO voy a usar pipe
+test.skip("pipe passes the results of inc to dbl", () => {
+  const pipeline = pipe(inc, dbl);
+  //console.log(pipe(inc, dbl));
+  const result = pipeline(2);
+  expect(result).toBe(6);
+});
+
+test.skip("pipe passes the results of dbl to inc", () => {
+  const pipeline = pipe(dbl, inc);
+  const result = pipeline(2); 
+  expect(result).toBe(5);
 });
